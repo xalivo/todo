16e66c8757b77eb6889bc65a0815221f8c1b2664
xalivo <87896066+xalivo@users.noreply.github.com>
2025-09-14 19:10:35 +0200

    enabled persistent storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import {AppBar, Paper, Toolbar, Typography} from "@mui/material";
-
 import NavigationBar from "./components/NavigationBar.tsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {AppViews} from "./common/enums.ts";
 import TodoList from "./components/TodoList.tsx";
 import CreatePage from "./components/CreatePage.tsx";
 import SettingsPage from "./components/SettingsPage.tsx";
+import useTodoStore from "./store/TodoStore.ts";
 
 const App = () => {
     const [view, setView] = useState<AppViews>(AppViews.Home);
+    const {todoItems, setTodoItems} = useTodoStore();
 
     const render = () => {
         switch (view) {
@@ -21,21 +22,45 @@ const App = () => {
         }
     }
 
-    return (<>
-        <AppBar position={"sticky"} >
-            <Toolbar>
-                <Typography variant="h5" component="div" sx={{flexGrow: 1}}>
-                    todo
-                </Typography>
-            </Toolbar>
-        </AppBar>
-        <div style={{display: "flex", justifyContent: "center"}}>
-            {render()}
-        </div>
-        <Paper sx={{position: 'fixed', bottom: 0, left: 0, right: 0}} elevation={3}>
-            <NavigationBar view={view} setView={setView}/>
-        </Paper>
-    </>);
+    const handleOnCloseWindow = () => {
+        localStorage.setItem("todoList", JSON.stringify(todoItems));
+    }
+
+    const handleOnOpenWindow = () => {
+        const storedItems = localStorage.getItem("todoList");
+        if (storedItems) {
+            setTodoItems(JSON.parse(storedItems));
+        }
+    }
+
+    useEffect(() => {
+        handleOnOpenWindow();
+    }, []);
+
+    useEffect(() => {
+        window.addEventListener("pagehide", handleOnCloseWindow);
+        return () => {
+            window.removeEventListener("pagehide", handleOnCloseWindow);
+        };
+    }, [todoItems]);
+
+
+    return (
+        <>
+            <AppBar position={"sticky"}>
+                <Toolbar>
+                    <Typography variant="h5" component="div" sx={{flexGrow: 1}}>
+                        todo
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <div style={{display: "flex", justifyContent: "center"}}>
+                {render()}
+            </div>
+            <Paper sx={{position: 'fixed', bottom: 0, left: 0, right: 0}} elevation={3}>
+                <NavigationBar view={view} setView={setView}/>
+            </Paper>
+        </>);
 }
 
 export default App;
