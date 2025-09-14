cdf42791f260acc6abf1478f9afac2e0659faf5e
xalivo <87896066+xalivo@users.noreply.github.com>
2025-09-14 20:06:09 +0200

    created constants file, implemented App card for SettingsPage

---
+++ src/common/constants.ts
@@ -0,0 +1,2 @@
+export const APP_URL = "http://localhost:5173/"
+export const APP_VERSION = "v1.0"
--- src/components/SettingsPage.tsx
+++ src/components/SettingsPage.tsx
@@ -1,12 +1,33 @@
-import {Typography} from "@mui/material";
+import {Button, Card, CardActions, CardContent, Snackbar, Typography} from "@mui/material";
+import {APP_URL, APP_VERSION} from "../common/constants.ts";
+import {useState} from "react";
 
 const SettingsPage = () => {
+    const [isSnackbarOpened, setIsSnackbarOpened] = useState<boolean>(false);
+    const [msg, setMsg] = useState<string>();
+
+    const sendMessage = (message: string) => {
+        setMsg(message);
+        setIsSnackbarOpened(true);
+    }
+
     return (
-        <div style={{ width: '100%', height: '100vh', background: 'black', color: 'white' }}>
-            <Typography>Settings</Typography>
-            <Typography>Settings</Typography>
-            <Typography>Settings</Typography>
-            <Typography>Settings</Typography>
+        <div style={{width: '100%'}}>
+            <Card>
+                <CardContent>
+                    <Typography variant={"h5"}>todo</Typography>
+                    <Typography variant={"body2"}>{APP_VERSION} - by xalivo</Typography>
+                </CardContent>
+                <CardActions>
+                    <Button variant={"outlined"} size={"small"}>GitHub</Button>
+                    <Button onClick={() => {navigator.clipboard.writeText(APP_URL); sendMessage("Copied to clipboard.")}} variant={"outlined"}
+                            size={"small"}>Share</Button>
+                </CardActions>
+            </Card>
+            <Snackbar open={isSnackbarOpened}
+                      autoHideDuration={1000}
+                      onClose={() => setIsSnackbarOpened(false)}
+                      message={msg}/>
         </div>
     );
 };
